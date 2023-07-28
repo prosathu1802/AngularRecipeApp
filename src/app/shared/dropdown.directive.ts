17375e71a13c5f660fccdcae7d89191f93dd6fe3
Hin Pham <hinphamd0218@gmail.com>
2023-07-27 22:35:30 -0500

    Third Step: Add dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, HostBinding, HostListener, OnInit } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective implements OnInit {
+    @HostBinding('class.open') isOpen: boolean = false;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        console.log(event.target);
+        console.log(this.elRef.nativeElement);
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+      }
+
+    constructor(private elRef: ElementRef) {}
+
+    ngOnInit(): void {
+    }
+
+    // @HostListener('click') toggleOpen(eventData: Event) {
+    //     this.isOpen = !this.isOpen;
+    // }
+
+}
